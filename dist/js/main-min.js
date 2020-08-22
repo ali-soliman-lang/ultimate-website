799a2aee250c46eb9e91bbfb21941be3384c7570
ali soliman <ali.eldinsdk@gmail.com>
2020-08-23 00:30:39 +0200

    lesson 12,13,14

--- dist/js/main-min.js
+++ dist/js/main-min.js
@@ -1,1 +1,1 @@
-$(function(){"use strict";$(".toggle-sidebar").on("click",function(){$(".content-area , .sidebar").toggleClass("no-sidebar")})});const myMap=new Map,mySet=new Set;
+$(function(){"use strict";$(".toggle-sidebar").on("click",function(){$(".content-area , .sidebar").toggleClass("no-sidebar")}),$(".toggle-submenu").on("click",function(){$(this).find(".fa-angle-right").toggleClass("down"),$(this).next(".child-links").slideToggle(),console.log("how fe eh msh fahem msh rady yshta8l leee")})});const myMap=new Map,mySet=new Set;
--- dist/js/main.js
+++ dist/js/main.js
@@ -4,6 +4,12 @@ $(function () {
   $(".toggle-sidebar").on("click", function () {
     $(".content-area , .sidebar").toggleClass("no-sidebar");
   });
+  // toggle submenu
+  $(".toggle-submenu").on("click", function () {
+    $(this).find(".fa-angle-right").toggleClass("down");
+    $(this).next(".child-links").slideToggle();
+    console.log("how fe eh msh fahem msh rady yshta8l leee");
+  });
 });
 
 const myMap = new Map();
