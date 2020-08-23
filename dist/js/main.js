aab5534ca77d8e2e683e93e1b9f2f8e616ecf9c3
ali soliman <ali.eldinsdk@gmail.com>
2020-08-23 16:27:15 +0200

    lesson 18

--- dist/js/main.js
+++ dist/js/main.js
@@ -10,7 +10,48 @@ $(function () {
     $(this).next(".child-links").slideToggle();
     console.log("how fe eh msh fahem msh rady yshta8l leee");
   });
+  // open close ful screen
+  $(".toggle-fullscreen").on("click", function () {
+    $(this).toggleClass("full-screen");
+    if ($(this).hasClass("full-screen")) {
+      // page is now full screen
+      openFullscreen();
+    } else {
+      // page is not full screen
+      closeFullscreen();
+    }
+  });
 });
+
+var elem = document.documentElement;
+
+/* Function to open fullscreen mode */
+function openFullscreen() {
+  if (elem.requestFullscreen) {
+    elem.requestFullscreen();
+  } else if (elem.mozRequestFullScreen) {
+    /* Firefox */
+    elem.mozRequestFullScreen();
+  } else if (elem.webkitRequestFullscreen) {
+    /* Chrome, Safari & Opera */
+    elem.webkitRequestFullscreen();
+  } else if (elem.msRequestFullscreen) {
+    /* IE/Edge */
+    elem.msRequestFullscreen();
+  }
+}
+
+function closeFullscreen() {
+  if (document.exitFullscreen) {
+    document.exitFullscreen();
+  } else if (document.mozCancelFullScreen) {
+    document.mozCancelFullScreen();
+  } else if (document.webkitExitFullscreen) {
+    document.webkitExitFullscreen();
+  } else if (document.msExitFullscreen) {
+    window.top.document.msExitFullscreen();
+  }
+}
 
 const myMap = new Map();
 
